394b5103100b079363a9cd6ebc8e42e6c3a2cae5
Younès Bouchouk <younes.78.alg@gmail.com>
2025-11-05 11:50:03 +0100

    create all features folders + add all view file + update build.gradle with retrofit

---
+++ app/src/main/java/com/example/scanner/features/resultScan/ResultScanView.kt
@@ -0,0 +1,39 @@
+package com.example.scanner.features.resultScan
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.scanner.ui.theme.ScannerTheme
+
+@Composable
+fun ResultScanView() {
+    Scaffold() { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text(text = "Page résultat du scan", fontSize = 20.sp)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ResultScanViewPreview(){
+    ScannerTheme() {
+        ResultScanView()
+    }
+}
